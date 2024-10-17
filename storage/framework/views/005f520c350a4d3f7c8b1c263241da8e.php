4d7b9636bafc8b7cf5d33dffa566f6c08e4f555d
asdthehiro <asdthehiro13@gmail.com>
2024-10-17 22:16:36 +0300

    Remove sensitive files and add to .gitignore

--- storage/framework/views/005f520c350a4d3f7c8b1c263241da8e.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /home/u389321274/domains/aldaarerp.com/public_html/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/c3144e4ba47b2a4f4f995b50a55f1582.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /home/u188192483/domains/techzone.so/public_html/aldaar/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
