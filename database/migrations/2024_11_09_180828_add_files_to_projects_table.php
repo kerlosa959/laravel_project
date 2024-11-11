1bafc105681bde3825e5d153eae959a903f28dfc
asdthehiro <asdthehiro13@gmail.com>
2024-11-11 02:21:57 +0200

    Update 2024_11_09_180828_add_files_to_projects_table.php

--- database/migrations/2024_11_09_180828_add_files_to_projects_table.php
+++ database/migrations/2024_11_09_180828_add_files_to_projects_table.php
@@ -4,25 +4,46 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::table('projects', function (Blueprint $table) {
-            //
-        });
+        if(!Schema::hasTable('projects')){
+            Schema::create('projects', function (Blueprint $table) {
+                $table->id();
+                $table->string('project_name');
+                $table->date('start_date')->nullable();
+                $table->date('end_date')->nullable();
+                $table->string('project_image')->nullable();
+                $table->string('project_excel')->nullable(); // Path to the Excel file
+                $table->string('project_word')->nullable(); // Path to the Word document
+                $table->string('project_pdf')->nullable(); // Path to the PDF document
+                $table->integer('budget')->nullable();
+                $table->integer('client_id');
+                $table->text('description')->nullable();
+                $table->string('status');
+                $table->string('estimated_hrs')->nullable();
+                $table->string('password')->nullable();
+                $table->text('copylinksetting')->nullable();
+                $table->text('tags')->nullable();
+                $table->unsignedBigInteger('created_by');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::table('projects', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('pojects');
     }
-};
+}
