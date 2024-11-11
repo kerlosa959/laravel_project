1aec1da8185dbdcafdfb1504fcbe7e44ff2bd25d
asdthehiro <asdthehiro13@gmail.com>
2024-11-11 02:38:58 +0200

    Update 2024_11_09_180828_add_files_to_projects_table.php

--- database/migrations/2024_11_09_180828_add_files_to_projects_table.php
+++ database/migrations/2024_11_09_180828_add_files_to_projects_table.php
@@ -4,46 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsTable extends Migration
+class AddFilesToProjectsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function upfliles()
+    public function up()
     {
-        if(!Schema::hasTable('projects')){
-            Schema::create('projects', function (Blueprint $table) {
-                $table->id();
-                $table->string('project_name');
-                $table->date('start_date')->nullable();
-                $table->date('end_date')->nullable();
-                $table->string('project_image')->nullable();
-                $table->string('project_excel')->nullable(); // Path to the Excel file
-                $table->string('project_word')->nullable(); // Path to the Word document
-                $table->string('project_pdf')->nullable(); // Path to the PDF document
-                $table->integer('budget')->nullable();
-                $table->integer('client_id');
-                $table->text('description')->nullable();
-                $table->string('status');
-                $table->string('estimated_hrs')->nullable();
-                $table->string('password')->nullable();
-                $table->text('copylinksetting')->nullable();
-                $table->text('tags')->nullable();
-                $table->unsignedBigInteger('created_by');
-                $table->timestamps();
-            });
-        }
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('project_image')->nullable();
+            $table->string('project_excel')->nullable(); // Path to the Excel file
+            $table->string('project_word')->nullable(); // Path to the Word document
+            $table->string('project_pdf')->nullable(); // Path to the PDF document
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('pojects');
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn(['project_image', 'project_excel', 'project_word', 'project_pdf']);
+        });
     }
 }
