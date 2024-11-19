6fbf6828d164d2b241c087562d37fc5b4c4e6c35
asdthehiro <asdthehiro13@gmail.com>
2024-11-19 02:25:45 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -445,6 +445,11 @@ body {
         <label for="email">Email <span class="required-star">*</span></label>
         <input name="email" type="text" placeholder="email@website.com" id="email">
       </div>
+      @error('email')
+                    <span class="error invalid-email text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
       <div class="login-form-group">
         <label for="pwd">Password <span class="required-star">*</span></label>
         <input name="password" autocomplete="off" type="password" placeholder="Minimum 8 characters" id="pwd">
