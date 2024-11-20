df0caf9d429a24c90a2e020979cc2488eccbbc39
kerlosa959 <151732322+kerlosa959@users.noreply.github.com>
2024-11-20 11:33:49 +0200

    form

--- resources/views/partials/admin/menu.blade.php
+++ resources/views/partials/admin/menu.blade.php
@@ -175,6 +175,7 @@
  @if (
     \Auth::user()->type != 'super admin' &&
         (Gate::check('manage user') || Gate::check('manage role') || Gate::check('manage client')))
+        
     <li
         class="dash-item dash-hasmenu {{ Request::segment(1) == 'users' ||
         Request::segment(1) == 'roles' ||
