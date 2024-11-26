eee9a75d70e05868b4ae8b172fe7c508f375d46e
kerlosa959 <151732322+kerlosa959@users.noreply.github.com>
2024-11-26 11:05:37 +0200

    Update index.blade.php

--- resources/views/projectworkerrequest/index.blade.php
+++ resources/views/projectworkerrequest/index.blade.php
@@ -14,7 +14,7 @@
 
         <nav class="navbar bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('labours.search') }}">search</a>
+                {{-- <a class="navbar-brand" href="{{ route('labours.search') }}">search</a> --}}
               <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
